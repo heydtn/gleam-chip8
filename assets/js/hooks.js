f5bac982d6b236ea58c2dd743fb5770302d939c4
Nate Heydt <heydtn@gmail.com>
2020-08-21 08:55:09 -0700

    Add sound hook

---
+++ assets/js/hooks.js
@@ -0,0 +1,45 @@
+let Hooks = {};
+
+const createAudio = () => {
+    const context = new AudioContext();
+    const oscillator = context.createOscillator();
+    oscillator.type = "triangle";
+    oscillator.frequency.value = 432;
+
+    const gain = context.createGain();
+    gain.gain.value = 0;
+
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+
+    oscillator.start(0);
+
+    return {
+        on: () => gain.gain.value = 0.2,
+        off: () => gain.gain.value = 0,
+        close: () => context.close()
+    }
+}
+
+Hooks.SoundCard = {
+    mounted() {
+        this.synth = createAudio();
+        this.handleEvent("soundcard_emit", emit => {
+            if (emit) {
+                this.synth.on();
+            } else {
+                this.synth.off();
+            }
+        });
+    },
+    beforeDestroy() {
+        this.synth.close();
+    },
+    disconnected() {
+        this.synth.off();
+    }
+}
+
+export {
+    Hooks
+};
